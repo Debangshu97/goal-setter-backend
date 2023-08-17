371c0243fa10296d0dd7ffc2afe5450aece2cbc8
Debangshu <kanthadebangshu97@gmail.com>
2023-08-17 21:31:51 +0530

    test

--- server/server.js
+++ server/server.js
@@ -10,6 +10,13 @@ connectDB()
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  // Replace * with the specific origin you want to allow, or use a list of allowed origins
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/goals',require("./routes/goalRoutes"));
 app.use('/api/users',require("./routes/userRoutes"));

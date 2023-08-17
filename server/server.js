7852c9a44a5ced140ffa80e551561b3dad20a81d
Debangshu <kanthadebangshu97@gmail.com>
2023-08-17 21:49:06 +0530

    test3

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   // Replace * with the specific origin you want to allow, or use a list of allowed origins
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 app.use(express.urlencoded({ extended: false }));
